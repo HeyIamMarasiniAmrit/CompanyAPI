448e7e7d68d19ecfcf897e46122f5c5ba02f8427
Amrit Marasini <101445427+HeyIamMarasiniAmrit@users.noreply.github.com>
2025-07-20 13:02:02 +0545

    Update views.py

--- api/views.py
+++ api/views.py
@@ -1,10 +1,24 @@
-from django.shortcuts import render
-from rest_framework import viewsets
-from api.models import Company
-from api.serializers import CompanySerializer
-
+from api.models import Company, Employee
+from api.serializers import CompanySerializer, EmployeeSerializer
+from rest_framework.decorators import action
 # Create your views here.
 class CompanyViewSet(viewsets.ModelViewSet):
     queryset = Company.objects.all()
-    serializer_class = CompanySerializer()
+    serializer_class = CompanySerializer
+
+    @action(detail=True,methods=['get'])
+    def employees(self,request,pk=None):
+        pass
+
+# class MyViewSet(viewsets.ModelViewSet):
+#     def get_serializer_class(self):
+#         if self.action == 'list':
+#             return CompanyListSerializer
+#         return CompanyDetailSerializer  # Returns the class, not an instance
+
+class EmployeeViewSet(viewsets.ModelViewSet):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
+
 
