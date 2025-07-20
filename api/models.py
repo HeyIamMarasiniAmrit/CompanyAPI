b3afbd588711348ee4eb0b9fe44b80a243753bb8
Amrit Marasini <101445427+HeyIamMarasiniAmrit@users.noreply.github.com>
2025-07-20 13:03:53 +0545

    Update models.py

--- api/models.py
+++ api/models.py
@@ -13,5 +13,18 @@ class Company(models.Model):
     added_date = models.DateField(auto_now=True)
     active = models.BooleanField(default=True)
 
-
+    def __str__(self):
+        return self.name + '--' + self.location
 #Employee Model
+class Employee(models.Model):
+    name = models.CharField(max_length=500)
+    email=models.CharField(max_length=300)
+    address = models.CharField(max_length=200)
+    phone = models.CharField(max_length=50)
+    about = models.TextField()
+    position = models.CharField(max_length=50, choices=(
+        ('manager', 'manager'),
+        ('software Developer', 'sd'),
+        ('project leader', 'pl')
+    ))
+    Company = models.ForeignKey(Company,on_delete=models.CASCADE)
